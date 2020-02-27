d73b784450059203ebd2578ec729debeee4e3f9a
Aleksandar_Stanojevic <aleksandar.stanojevic.1509@gmail.com>
2020-02-27 13:34:49 +0100

    deleted scroll to top function

--- main.js
+++ main.js
@@ -71,19 +71,3 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 720 || document.documentElement.scrollTop > 720) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
